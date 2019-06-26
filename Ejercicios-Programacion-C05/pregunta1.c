34b06a207391f7535f2b24cb65472d8166ccd05e
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-06-26 18:14:39 -0500

    Update pregunta1.c

--- Ejercicios-Programacion-C05/pregunta1.c
+++ Ejercicios-Programacion-C05/pregunta1.c
@@ -1,5 +1,27 @@
-// Write a program that calls fork(). Before calling fork(), have the
-// main process access a variable (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
-// What happens to the variable when both the child and parent change
-// the value of x?
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+int main()
+{
+    int x;
+    x = 100;
+    int rc = fork();
+
+    if (rc == 0) // proceso hijo
+    {
+        printf("Proceso hijo\n");
+        printf("valor de x: %d\n", x);
+        x = 200;
+        printf("nuevo de valor de x: %d\n", x);
+        
+    }
+    else if (rc > 0) // proceso padre
+    {
+        wait(NULL);
+        printf("proceso padre\n");
+        printf("valor de x: %d\n", x);
+        x = 300;
+        printf("nuevo valor de x: %d\n", x);
+    }
+}
