984ab41363b09c80f7ffaf7c3e34a64a8fe6aa3b
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-06-26 09:39:04 -0500

    Create pregunta2.c

---
+++ Ejercicios-Programacion-C05/pregunta2.c
@@ -0,0 +1,5 @@
+// Write a program that opens a file (with the open() system call)
+// and then calls fork() to create a new process. Can both the child
+// and parent access the file descriptor returned by open()? What
+// happens when they are writing to the file concurrently, i.e., at the
+// same time?
