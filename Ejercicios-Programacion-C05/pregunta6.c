4d3bd7532e73ce7656991d229d4f3ee5f74af70f
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-06-26 19:36:24 -0500

    Update pregunta6.c

--- Ejercicios-Programacion-C05/pregunta6.c
+++ Ejercicios-Programacion-C05/pregunta6.c
@@ -1,1 +1,21 @@
-holi6
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main()
+{
+    int rc = fork();
+    
+    if (rc == 0) // proceso hijo
+    {
+        printf("soy el proceso hijo y me identificador es: %d\n", getpid());
+        exit(0);
+    }
+    else if (rc > 0) // proceso padre
+    {
+        int status;
+        waitpid(rc, &status, 0);
+        printf("soy el proceso padre y mi identificador es: %d\n", getpid());        
+    }
+}
