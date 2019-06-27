e6820e121c815c8da36f1c7f4c19699ab591f9af
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-06-26 19:31:18 -0500

    Update pregunta5.c

--- Ejercicios-Programacion-C05/pregunta5.c
+++ Ejercicios-Programacion-C05/pregunta5.c
@@ -1,1 +1,19 @@
-holi5
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int rc = fork();
+
+    if (rc == 0) // proceso hijo
+    {
+        int miEspera = wait(NULL);
+        printf("proceso hijo\n");
+        printf("el codigo de retorno de wait() es %d\n", miEspera);
+    }
+    else if (rc > 0) // proceso padre
+    {
+        printf("proceso padre\n");
+    }
+}
