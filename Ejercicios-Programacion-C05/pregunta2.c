eedba25be901612b401c4adf470083767463b178
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-06-26 19:07:40 -0500

    Update pregunta2.c

--- Ejercicios-Programacion-C05/pregunta2.c
+++ Ejercicios-Programacion-C05/pregunta2.c
@@ -9,31 +9,31 @@
 int main()
 {
     struct stat sb;
-    int my_file = open("hello.txt", O_CREAT | O_RDWR, S_IRWXU);
+    int miArchivo = open("miArchivo.txt", O_CREAT | O_RDWR, S_IRWXU);
     
     int rc = fork();
 
     if (rc == 0)
     {
         const char * child_msg = "Hello, I am child process. Writing on you\n";
-        printf("************ Child process writing on file ************\n");
-        write(my_file, child_msg , strlen(child_msg));
+        printf("proceso hijo escribiendo en el archivo\n");
+        write(miArchivo, child_msg , strlen(child_msg));
         exit(0);
     }
     else if (rc > 0)
     {
         wait(NULL);
         const char * parent_msg = "Hello, I am parent process. Writing on you\n";
-        printf("************ Parent process writing on file ************\n");
-        write(my_file, parent_msg, strlen(parent_msg));
+        printf("proceso padre escribiendo en el archivo\n");
+        write(miArchivo, parent_msg, strlen(parent_msg));
     }
     sync();
-    fstat(my_file, &sb);
-    lseek(my_file, 0, SEEK_SET);
+    fstat(miArchivo, &sb);
+    lseek(miArchivo, 0, SEEK_SET);
     char * buffer = malloc(sb.st_size);
-    read(my_file, buffer, sb.st_size);
+    read(miArchivo, buffer, sb.st_size);
 
     printf("Content of file: \n%s", buffer);
-    close(my_file);
+    close(miArchivo);
     return 0;
 }
