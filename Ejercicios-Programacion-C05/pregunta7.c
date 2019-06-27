912c8b78d9f0e8b889a4e6dca9d90b60eb13ee45
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-06-26 19:42:39 -0500

    Update pregunta7.c

--- Ejercicios-Programacion-C05/pregunta7.c
+++ Ejercicios-Programacion-C05/pregunta7.c
@@ -1,1 +1,19 @@
-holi7
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int rc = fork();
+
+    if (rc == 0) // child process
+    {
+        close(STDOUT_FILENO);
+        printf("Hello World. Can you see this?\n");
+    }
+    else if (rc > 0)
+    {
+        wait(NULL);
+        printf("I am parent process\n");
+    }
+}
