bd2d003168afc5a1979374ab106528b33530f8ff
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-07-10 11:13:31 -0500

    Update pregunta3.c

--- Ejercicios-Programacion-C05/pregunta3.c
+++ Ejercicios-Programacion-C05/pregunta3.c
@@ -11,16 +11,14 @@ void sig_handler(int signum) {
 int main (int argc, char const *argv[]) {
     int pid_padre = getpid();
     int rc = fork();
-    if (rc == 0) //child
+    if (rc == 0) // proceso hijo
     {
         printf("hello\n");
         kill(pid_padre, SIGCONT);
         exit(0);
     }
-    else if (rc > 0) // parent
-    {
-        //  https://www.unix.com/302582537-post2.html?s=c29ed69cf8a6adc4c14984b71e3aa176
-        
+    else if (rc > 0) // proceso padre
+    {        
         signal(SIGCONT, sig_handler); 
         pause();
     }
