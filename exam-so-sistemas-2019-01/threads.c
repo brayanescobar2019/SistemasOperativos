56a4d6e3d582fe2bdd2f163aed4f724bc4aa8960
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-08-02 12:18:28 -0500

    Update threads.c

--- exam-so-sistemas-2019-01/threads.c
+++ exam-so-sistemas-2019-01/threads.c
@@ -32,10 +32,10 @@ int main(int argc, char *argv[]) {
     pthread_t p1, p2;
     crear_puerta(miPuerta);
     printf("Initial value : %d\n", counter);
-    Pthread_create(&p1, NULL, worker, NULL); 
-    Pthread_create(&p2, NULL, worker, NULL);
-    Pthread_join(p1, NULL);
-    Pthread_join(p2, NULL);
+    pthread_create(&p1, NULL, worker, NULL); 
+    pthread_create(&p2, NULL, worker, NULL);
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
     printf("Final value   : %d\n", counter);
     destruir_puerta(miPuerta);	
     tiempo_final = clock();
