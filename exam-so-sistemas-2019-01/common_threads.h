f61f7d85c13489220753e2b073dffee70c96a2fb
brayanescobar2019 <50996929+brayanescobar2019@users.noreply.github.com>
2019-08-02 12:17:05 -0500

    Update common_threads.h

--- exam-so-sistemas-2019-01/common_threads.h
+++ exam-so-sistemas-2019-01/common_threads.h
@@ -9,13 +9,13 @@
 #include <semaphore.h>
 #endif
 
-#define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
-#define Pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
+#define pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
+#define pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
 
-#define Pthread_mutex_lock(m)                            assert(pthread_mutex_lock(m) == 0);
-#define Pthread_mutex_unlock(m)                          assert(pthread_mutex_unlock(m) == 0);
-#define Pthread_cond_signal(cond)                        assert(pthread_cond_signal(cond) == 0);
-#define Pthread_cond_wait(cond, mutex)                   assert(pthread_cond_wait(cond, mutex) == 0);
+#define pthread_mutex_lock(m)                            assert(pthread_mutex_lock(m) == 0);
+#define pthread_mutex_unlock(m)                          assert(pthread_mutex_unlock(m) == 0);
+#define pthread_cond_signal(cond)                        assert(pthread_cond_signal(cond) == 0);
+#define pthread_cond_wait(cond, mutex)                   assert(pthread_cond_wait(cond, mutex) == 0);
 
 #define Mutex_init(m)                                    assert(pthread_mutex_init(m, NULL) == 0);
 #define Mutex_lock(m)                                    assert(pthread_mutex_lock(m) == 0);
